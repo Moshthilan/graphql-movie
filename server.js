77bd3169338ab3b0b325a61bddc68ec126b3f8d8
Mositha <thilan@approbus.com>
2022-04-21 23:44:43 +0530

    movie catalog changed

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ var movies = [
     release_date: 'June 15, 1994',
     synopsis:
       'Lion prince Simba and his father are targeted by his bitter uncle, who wants to ascend the throne himself.',
-    genre: 'Annimation',
+    genre: 'Animation',
     description:
       'A young lion prince is cast out of his pride by his cruel uncle, who claims he killed his father. While the uncle rules with an iron paw, the prince grows up beyond the Savannah, living by a philosophy: No worries for the rest of your days. But when his past comes to haunt him, the young prince must decide his fate: Will he remain an outcast or face his demons and become what he needs to be?',
   },
